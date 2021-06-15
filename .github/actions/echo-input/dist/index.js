2a6741541c43aca2bc67ee77f332b24e4fb84ab5
Simran Spiller <simran.spiller@gmail.com>
2021-06-16 00:24:55 +0200

    JSON encoding seems like the only way

--- .github/actions/echo-input/dist/index.js
+++ .github/actions/echo-input/dist/index.js
@@ -544,9 +544,12 @@ const core = __nccwpck_require__(186)
 
 try {
     const input = core.getInput("input")
+    const json = core.getInput("json")
     console.log(`Got input: '${input}'`)
+    console.log(`Got json: '${json}'`)
     core.setOutput("output", input)
     core.setOutput("json", JSON.stringify(input))
+    core.setOutput("parsedJson", JSON.parse(json))
 } catch (error) {
     core.setFailed(error.message)
 }
--- .github/actions/echo-input/index.js
+++ .github/actions/echo-input/index.js
@@ -2,9 +2,12 @@ const core = require("@actions/core")
 
 try {
     const input = core.getInput("input")
+    const json = core.getInput("json")
     console.log(`Got input: '${input}'`)
+    console.log(`Got json: '${json}'`)
     core.setOutput("output", input)
     core.setOutput("json", JSON.stringify(input))
+    core.setOutput("parsedJson", JSON.parse(json))
 } catch (error) {
     core.setFailed(error.message)
 }
