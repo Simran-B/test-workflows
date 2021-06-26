cff25bc52fe53154f4cb470702fbdb747393d3f8
Simran Spiller <simran.spiller@gmail.com>
2021-06-27 00:20:56 +0200

    Test trimWhiteSpace: false

--- .github/actions/echo-input/dist/index.js
+++ .github/actions/echo-input/dist/index.js
@@ -545,9 +545,12 @@ const core = __nccwpck_require__(186)
 try {
     const input = core.getInput("input")
     const json = core.getInput("json")
+    const noTrim = core.getInput("input", { trimWhitespace: false })
     console.log(`Got input: '${input}'`)
+    console.log(`Untrimmed: '${noTrim}'`)
     console.log(`Got json: '${json}'`)
     core.setOutput("output", input)
+    core.setOutput("outputRaw", noTrim)
     core.setOutput("json", JSON.stringify(input))
     if (json) {
         core.setOutput("parsedJson", JSON.parse(json))
--- .github/actions/echo-input/index.js
+++ .github/actions/echo-input/index.js
@@ -3,9 +3,12 @@ const core = require("@actions/core")
 try {
     const input = core.getInput("input")
     const json = core.getInput("json")
+    const noTrim = core.getInput("input", { trimWhitespace: false })
     console.log(`Got input: '${input}'`)
+    console.log(`Untrimmed: '${noTrim}'`)
     console.log(`Got json: '${json}'`)
     core.setOutput("output", input)
+    core.setOutput("outputRaw", noTrim)
     core.setOutput("json", JSON.stringify(input))
     if (json) {
         core.setOutput("parsedJson", JSON.parse(json))
